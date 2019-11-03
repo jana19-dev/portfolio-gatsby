b7f5de35b01c9fa327c474667c8911dafdcad319
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2019-11-02 23:28:38 -0400

    Adjust styling of parallax layout

---
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Parallax } from "react-spring/renderprops-addons.cjs"
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
+import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero"
+import Projects from "@lekoarts/gatsby-theme-cara/src/components/projects"
+import About from "@lekoarts/gatsby-theme-cara/src/components/about"
+import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact"
+
+const Cara = () => {
+  const isMobile = window.innerWidth <= 450 
+  return (
+    <Layout>
+      <Parallax pages={isMobile ? 7.2 : 5}>
+        <Hero offset={0} />
+        <Projects offset={isMobile ? 2.5 : 1} />
+        <About offset={isMobile ? 5.8 : 3} />
+        <Contact offset={isMobile ? 6.2 : 4} />
+      </Parallax>
+    </Layout>
+  )
+}
+
+export default Cara
